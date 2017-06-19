a9f8b947843a4f8b76b0106511c3fdef7dde908c
Danny Suazo <dannysuazor@yahoo.com>
2017-06-19 02:45:55 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# sequelizedBurger
+# burger
+burger app
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Texas Chainsaw Massa-Curd Burger');
+INSERT INTO burgers (burger_name) VALUES ('Sweet Home Avocado Burger');
+INSERT INTO burgers (burger_name) VALUES ('Kales From the Crypt Burger');
